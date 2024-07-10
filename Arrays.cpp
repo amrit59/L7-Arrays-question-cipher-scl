557fd4d9523b47868af21ee9e58ebe09db1faeca
Amrit Raj <121791502+amrit59@users.noreply.github.com>
2024-07-10 23:04:31 +0530

    Add files via upload

---
+++ Arrays.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int a,b,c,d,e;   // Array can store multiple value in a single varibale name.
+			  //The indexing of array start from 0 not 1
+	cout<<"Give 5 integers as input"<<endl;
+	cin>>a>>b>>c>>d>>e;
+	cout<<"The integers in reverse order are: "<<a<<b<<c<<d<<e;
+	return 0;
+	
+}
